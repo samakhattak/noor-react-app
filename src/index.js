95170c9a6e0602bd457d1f6ca1db4a865c71f0b8
Noor Us Sama Tariq <168010773+samakhattak@users.noreply.github.com>
2025-05-14 14:55:38 +0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => <h1>Welcome to Noor's React App on Amplify!</h1>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
